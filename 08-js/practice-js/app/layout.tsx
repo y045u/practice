448c895ab75835b19eaa07f76b56a5d5a9a35683
y045u <fukasawa0405.tech@gmail.com>
2024-04-30 01:50:27 +0900

    08 layouttsx

--- 08-js/practice-js/app/layout.tsx
+++ 08-js/practice-js/app/layout.tsx
@@ -15,8 +15,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="ja">
+      <body className={inter.className}>
+        <header className="">タイトル</header>
+        {children}
+      </body>
     </html>
   );
 }
